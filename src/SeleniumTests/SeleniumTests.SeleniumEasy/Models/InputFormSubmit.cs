d1a5dc42e40c854a4556c2895e7e8cf026816008
Dishant Arora <dishantarora@outlook.com>
2020-09-22 17:44:24 +0530

    Object of InputFormSubmit class will provide the xpaths at run time.

---
+++ src/SeleniumTests/SeleniumTests.SeleniumEasy/Models/InputFormSubmit.cs
@@ -0,0 +1,19 @@
+﻿namespace SeleniumTests.SeleniumEasy.Models
+{
+    sealed class InputFormSubmit
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNuber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string WebsiteOrDomainName { get; set; }
+        public string YesOptionForHosting { get; set; }
+        public string NoOptionForHosting { get; set; }
+        public string ProjectDescription { get; set; }
+        public string SendButton { get; set; }
+    }
+}
