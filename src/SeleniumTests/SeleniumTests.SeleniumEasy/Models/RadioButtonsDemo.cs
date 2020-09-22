1cb159b42b4579e02472957e04f8cede779c3c7a
Dishant Arora <dishantarora@outlook.com>
2020-09-22 17:44:59 +0530

    Object of RadioButtonsDemo class will provide the xpaths at run time.

---
+++ src/SeleniumTests/SeleniumTests.SeleniumEasy/Models/RadioButtonsDemo.cs
@@ -0,0 +1,17 @@
+﻿namespace SeleniumTests.SeleniumEasy.Models
+{
+    sealed class RadioButtonsDemo
+    {
+        public string SingleMaleRadioButton { get; set; }
+        public string SingleFemaleRadioButton { get; set; }
+        public string GetCheckedValueButton { get; set; }
+        public string ResultOfSingleRadioButtonDemo { get; set; }
+        public string GroupMaleRadioButton { get; set; }
+        public string GroupFemaleRadioButton { get; set; }
+        public string ZeroTo5AgeButton { get; set; }
+        public string FiveTo15Button { get; set; }
+        public string FifteenTo50Button { get; set; }
+        public string GetValuesButton { get; set; }
+        public string ResultOfGroupRadioButtonDemo { get; set; }
+    }
+}
