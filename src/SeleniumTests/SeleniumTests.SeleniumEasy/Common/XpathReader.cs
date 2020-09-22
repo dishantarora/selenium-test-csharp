6847f7e97645d4c69be50950a934673bbf2059db
Dishant Arora <dishantarora@outlook.com>
2020-09-22 17:42:41 +0530

    XpathReader contains the implementation for reading json which stores thw xpaths.

---
+++ src/SeleniumTests/SeleniumTests.SeleniumEasy/Common/XpathReader.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace SeleniumTests.SeleniumEasy.Common
+{
+    sealed class XpathReader : IXpathReader
+    {
+        private string FilePath { get; set; }
+        public XpathReader(string filePath) => FilePath = filePath;
+        public T GetValue<T>()
+        {
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(FilePath));
+        }
+    }
+}
