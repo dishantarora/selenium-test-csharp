2b98f60bb565bfab95f70d999525cba152fd5d5f
Dishant Arora <dishantarora@outlook.com>
2020-09-22 17:43:45 +0530

    Object of CheckboxDemo class will provide the xpaths at run time.

---
+++ src/SeleniumTests/SeleniumTests.SeleniumEasy/Models/CheckboxDemo.cs
@@ -0,0 +1,10 @@
+﻿namespace SeleniumTests.SeleniumEasy.Models
+{
+    sealed class CheckboxDemo
+    {
+        public string SingleCheckbox { get; set; }
+        public string SuccessMessage { get; set; }
+        public string Options { get; set; }
+        public string CheckAllButton { get; set; }
+    }
+}
