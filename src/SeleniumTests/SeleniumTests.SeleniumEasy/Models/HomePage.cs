cbe0533a8507cb8302e79e39b03e5b8bbc96e1f2
Dishant Arora <dishantarora@outlook.com>
2020-09-22 17:43:57 +0530

    Object of HomePage class will provide the xpaths at run time.

---
+++ src/SeleniumTests/SeleniumTests.SeleniumEasy/Models/HomePage.cs
@@ -0,0 +1,16 @@
+﻿namespace SeleniumTests.SeleniumEasy.Models
+{
+    sealed class HomePage
+    {
+        public string LightBox { get; set; }
+        public string LightBoxCloseIcon { get; set; }
+        public string InputFormsTab { get; set; }
+        public string SimpleFormDemo { get; set; }
+        public string CheckboxDemo { get; set; }
+        public string RadioButtonsDemo { get; set; }
+        public string DropdownList { get; set; }
+        public string InputFormSubmit { get; set; }
+        public string AjaxFormSubmit { get; set; }
+        public string JQuerySelectDropdown { get; set; }
+    }
+}
