89fd0ea3b524180e6b0fb51ee310da1e35ddf3dc
Dishant Arora <dishantarora@outlook.com>
2020-09-22 17:47:16 +0530

    Stores the web element of Ajax Form Submit web page and provides the action for creating automation tests.

---
+++ src/SeleniumTests/SeleniumTests.SeleniumEasy/PageObjects/AjaxFormSubmit.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SeleniumTests.SeleniumEasy.Common;
+
+namespace SeleniumTests.SeleniumEasy.PageObjects
+{
+    sealed class AjaxFormSubmit
+    {
+        private IWebDriver Driver { get; set; }
+        private IXpathReader XpathReader { get; set; }
+        private IWebElement Name => Driver.FindElement(
+            By.XPath(XpathReader.GetValue<Models.AjaxFormSubmit>().Name));
+        private IWebElement Comment => Driver.FindElement(
+            By.XPath(XpathReader.GetValue<Models.AjaxFormSubmit>().Comment));
+        private IWebElement SubmitButton => Driver.FindElement(
+            By.XPath(XpathReader.GetValue<Models.AjaxFormSubmit>().SubmitButton));
+        private IWebElement FormStatus => Driver.FindElement(
+            By.XPath(XpathReader.GetValue<Models.AjaxFormSubmit>().FormStatus));
+        public AjaxFormSubmit(IWebDriver driver,IXpathReader xpathReader)
+        {
+            Driver = driver;
+            XpathReader = xpathReader;
+        }
+        public void FillName(string value) => Name.SendKeys(value);
+        public void FillComment(string value) => Comment.SendKeys(value);
+        public void ClickSubmitButton() => SubmitButton.Click();
+        public string GetFormStatus() => FormStatus.Text;
+    }
+}
