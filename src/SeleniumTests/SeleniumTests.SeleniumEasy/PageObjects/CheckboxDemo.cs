476627a355d28e6339d0a082804990bd0401d2ba
Dishant Arora <dishantarora@outlook.com>
2020-09-22 17:47:32 +0530

    Stores the web element of Checkbox Demo web page and provides the action for creating automation tests.

---
+++ src/SeleniumTests/SeleniumTests.SeleniumEasy/PageObjects/CheckboxDemo.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using SeleniumTests.SeleniumEasy.Common;
+using System.Collections.ObjectModel;
+
+namespace SeleniumTests.SeleniumEasy.PageObjects
+{
+    sealed class CheckboxDemo
+    {
+        private IWebDriver Driver { get; set; }
+        private IXpathReader XpathReader { get; set; }
+
+        private IWebElement SingleCheckbox => Driver.FindElement(
+            By.XPath(XpathReader.GetValue<Models.CheckboxDemo>().SingleCheckbox));
+        private IWebElement SuccessMessage => Driver.FindElement(
+            By.XPath(XpathReader.GetValue<Models.CheckboxDemo>().SuccessMessage));
+        private ReadOnlyCollection<IWebElement> Options => Driver.FindElements(
+            By.XPath(XpathReader.GetValue<Models.CheckboxDemo>().Options));
+        private IWebElement CheckAllButton => Driver.FindElement(
+            By.XPath(XpathReader.GetValue<Models.CheckboxDemo>().CheckAllButton));
+
+        public CheckboxDemo(IWebDriver driver,
+                            IXpathReader xpathReader)
+        {
+            Driver = driver;
+            XpathReader = xpathReader;
+        }
+
+        public void CheckSingleCheckbox() => SingleCheckbox.Click();
+        public string GetSuccessMessage() => SuccessMessage.Text;
+        public void CheckOptions(int index) => Options[index].Click();
+        public void UnCheckAllOptions() => CheckAllButton.Click();
+    }
+}
